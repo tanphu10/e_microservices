4a33764d18816365c62a0c095c4ce0e35aba4a36
TanPhuphu1 <phantanphu10@gmail.com>
2024-05-30 16:45:55 +0700

    update Grpc inventory consume rev_11

---
+++ src/Services/Inventory/Inventory.Grpc/Services/InventoryService.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+using Inventory.Grpc.Protos;
+using Inventory.Grpc.Repositories.Interfaces;
+using ILogger = Serilog.ILogger;
+namespace Inventory.Grpc.Services
+{
+    public class InventoryService : StockProtoService.StockProtoServiceBase
+    {
+        private readonly IInventoryRepository _repository;
+        private readonly ILogger _logger;
+
+        public InventoryService(ILogger logger, IInventoryRepository repository)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+        public override async Task<StockModel> GetStock(GetStockRequest request, ServerCallContext context)
+        {
+            _logger.Information($"BEGIN Get Stock of itemNo :{request.ItemNo}");
+
+            var stockQuantity = await _repository.GetStockQuantity(request.ItemNo);
+            var result = new StockModel
+            {
+                Quantity = stockQuantity
+            };
+            _logger.Information($"END Get Stock of itemNo :{request.ItemNo}-Quantity:");
+            return result;
+
+        }
+    }
+}
